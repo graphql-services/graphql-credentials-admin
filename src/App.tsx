6530296f2e6b2c0b76ec3edc72df28f4eb5eaf6b
Jakub Knejzlik <jakub.knejzlik@inloop.eu>
2018-08-07 00:22:41 +0200

    Update layout to latest antd package

--- src/App.tsx
+++ src/App.tsx
@@ -8,47 +8,46 @@ import { userDetail } from './pages/user-detail';
 import { users } from './pages/users';
 
 const layout = (props: AuthContentProps) => (
-  <Layout
-    menu={[
-      <Layout.MenuItem key="/" title="Dashboard" icon="exclamation-circle-o" />,
-      <Layout.MenuItem key="/users/" title="Users" icon="user" />,
+  <Layout logout={props.logout}>
+    <Layout.Menu>
+      <Layout.MenuItem key="/" title="Dashboard" icon="exclamation-circle-o" />
+      <Layout.MenuItem key="/users/" title="Users" icon="user" />
       <Layout.MenuItem key="/roles/" title="Roles" icon="key" />
-    ]}
-    structure={[
+    </Layout.Menu>
+    <Layout.Structure>
       <Layout.StructureItem
         key="/"
         name="Dashboard"
         content={(route: RouteComponentProps<any>) => 'Hello world!'}
-      />,
+      />
       <Layout.StructureItem key="/users/" name="Users" content={users} />,
       <Layout.StructureItem
         key="/users/new"
         name="Create user"
         content={(route: RouteComponentProps<any>) => userDetail(route)}
-      />,
+      />
       <Layout.StructureItem
         key="/users/:id"
         name="Edit user"
         content={(route: RouteComponentProps<any>) =>
           userDetail(route, route.match.params.id)
         }
-      />,
+      />
       <Layout.StructureItem key="/roles/" name="Roles" content={roles} />,
       <Layout.StructureItem
         key="/roles/new"
         name="Create role"
         content={(route: RouteComponentProps<any>) => roleDetail(route)}
-      />,
+      />
       <Layout.StructureItem
         key="/roles/:id"
         name="Edit role"
         content={(route: RouteComponentProps<any>) =>
           roleDetail(route, route.match.params.id)
         }
-      />,
-    ]}
-    logout={props.logout}
-  />
+      />
+    </Layout.Structure>
+  </Layout>
 );
 
 export const content = (
